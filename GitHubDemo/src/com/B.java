651bc758e3565e1cec06c6535f66e86c3b83493a
DELL <DELL@192.168.100.6>
2023-05-16 10:57:09 +0530

    by abc

---
+++ GitHubDemo/src/com/B.java
@@ -0,0 +1,16 @@
+package com;
+
+public class B {
+	
+	public void m1()
+	{
+		System.out.println("Hi");
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+
+	}
+
+}
